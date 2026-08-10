545d1d66ea52af3124044c092b55b60c5c6a01dc
bielvicens <bielvicens05@gmail.com>
2026-08-10 19:18:48 +0200

    Dice improvement

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -4,10 +4,6 @@ from .trainer import Trainer, TrainingHistory
 from .unet import UNet3D
 from .losses import DiceCrossEntropyLoss
 
-self.scaler = torch.amp.GradScaler(
-    "cuda",
-    enabled=self.device.type == "cuda",
-)
 
 __all__ = [
     "UNet3D",
